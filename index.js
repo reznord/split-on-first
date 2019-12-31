55ae20d9b82334fae2e63818c76de2241dec0982
Anup <allamsetty.anup@gmail.com>
2019-12-31 08:57:20 +0530

    Use ES5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (string, separator) => {
+module.exports = function (string, separator) {
 	if (!(typeof string === 'string' && typeof separator === 'string')) {
 		throw new TypeError('Expected the arguments to be of type `string`');
 	}
